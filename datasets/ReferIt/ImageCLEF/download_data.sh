a008eb470d6b7dfd21b2a03dc901e991ff030e68
andrewliao11 <andrewliao11@gmail.com>
2016-06-29 16:32:32 +0800

    download

---
+++ datasets/ReferIt/ImageCLEF/download_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget ./referitdata.tar.gz http://www.eecs.berkeley.edu/~ronghang/projects/cvpr16_text_obj_retrieval/referitdata.tar.gz
+tar -xzvf ./referitdata.tar.gz
---
+++ datasets/ReferIt/ReferitData/download_ReferitData.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget ReferitData.zip http://tamaraberg.com/referitgame/ReferitData.zip
+unzip ReferitData.zip 
