7e8e87b3a408cbad0811ffe6df44ef54623ead8a
andrewliao11 <andrewliao11@gmail.com>
2016-06-29 13:36:40 +0800

    modify download file

--- models/download_trained_models.sh
+++ models/download_trained_models.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -O ./models/VGG_ILSVRC_16_layers.caffemodel http://www.eecs.berkeley.edu/~ronghang/projects/cvpr16_text_obj_retrieval/models/VGG_ILSVRC_16_layers.caffemodel
+wget ./models/VGG_ILSVRC_16_layers.caffemodel http://www.eecs.berkeley.edu/~ronghang/projects/cvpr16_text_obj_retrieval/models/VGG_ILSVRC_16_layers.caffemodel
