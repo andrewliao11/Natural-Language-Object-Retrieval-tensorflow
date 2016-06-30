1e38e75f201e22527abe694c67c01e3ba4f4d956
Andrew <andrewliao11@gmail.com>
2016-06-30 21:29:32 +0800

    Delete download_referit_dataset.sh

--- datasets/download_referit_dataset.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget http://tamaraberg.com/referitgame/ReferitData.zip
-unzip ./ReferIt/ReferitData/ReferitData.zip 
-wget http://www.eecs.berkeley.edu/~ronghang/projects/cvpr16_text_obj_retrieval/referitdata.tar.gz
-tar -xzvf ./ReferIt/ImageCLEF/referitdata.tar.gz
